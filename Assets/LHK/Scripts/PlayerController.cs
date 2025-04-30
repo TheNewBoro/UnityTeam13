620610ddd423ec2f3b57c8c61da210f8ebeef851
antactics <142508000+antactics@users.noreply.github.com>
2025-04-30 15:18:45 +0900

    [feat] player run
    playercontroller스크립트에
    left 시프트를 누르는 동안 달리기 이동속도로 이동하도록 기능추가

--- Assets/LHK/Scripts/PlayerController.cs
+++ Assets/LHK/Scripts/PlayerController.cs
@@ -9,7 +9,9 @@ public class PlayerController : MonoBehaviour
 
     //플레이어 체력
     [SerializeField] private float playerHP;
-    //적 또는 적의 발사체와 충돌하면 hp-1
+
+    //플레이어 이동속도
+    [SerializeField] private float playerMoveSpeed;
     
     
 
@@ -22,6 +24,8 @@ public class PlayerController : MonoBehaviour
 
     }
 
+
+    //적 또는 적의 발사체와 충돌하면 hp-1
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Bullet"))

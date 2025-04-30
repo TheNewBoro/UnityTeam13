b466a83f1de10d9b3f20e07c50a50f946e6299f2
TheNewBoro <potar1132@gmail.com>
2025-04-30 17:40:01 +0900

    [Feat]Enemy component
    적의 플레이어 탐지 컴포넌트 추가

--- Assets/Kang_MH(Lead)/Scripts/Player_Tracker.cs
+++ Assets/Kang_MH(Lead)/Scripts/Player_Tracker.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player_Tracker : MonoBehaviour
 {
+    [SerializeField] private float rotSpeed;
+    [SerializeField] private float detectedRadius;
+    // Todo: 나중에 지우고 변경 맵 오브젝트가 열렸을 때 감지하게 끔 수정 및 사거리 안에 들어왔을 때 멈추게 끔 변경
+    [SerializeField] private LayerMask target;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,31 @@ public class Player_Tracker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DetectedTarget();
     }
+
+
+    private void DetectedTarget()
+    {
+        if(Physics.OverlapSphere(transform.position, detectedRadius, target).Length > 0)
+        { 
+        
+        }
+        else
+        {
+            EnemyRotate();
+        }
+    }
+    
+    private void EnemyRotate()
+    {
+        transform.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, detectedRadius);
+    }
+
 }

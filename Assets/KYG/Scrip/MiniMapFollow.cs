df3ef32f9f701f6408230acfd0ec067ac29cf772
COrollaROOT <doctor6749@gmail.com>
2025-05-09 15:53:36 +0900

    [feat] minimap UI 구현
    [feat] minimap UI 구현

---
+++ Assets/KYG/Scrip/MiniMapFollow.cs
@@ -0,0 +1,11 @@
+public class MiniMapFollow : MonoBehaviour
+{
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPos = player.position;
+        newPos.y = transform.position.y;
+        transform.position = newPos;
+    }
+}
